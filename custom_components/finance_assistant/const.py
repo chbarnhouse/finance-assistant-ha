2bb852981550d1ae97c6f12b8b2ba3436e77789b
chbarnhouse <chbarnhouse@gmail.com>
2025-08-06 19:25:41 -0500

    Bump version to 0.14.64

--- custom_components/finance_assistant/const.py
+++ custom_components/finance_assistant/const.py
@@ -32,5 +32,5 @@ DEVICE_INFO = {
     "name": "Finance Assistant",
     "manufacturer": "Finance Assistant",
     "model": "Finance Assistant",
-    "sw_version": "0.14.63",
+    "sw_version": "0.14.64",
 } 
